4f94bb76f2a61fa4f552432f516d03fcc827a2e6
Nikitosssssss <112859272+Nikitosssssss@users.noreply.github.com>
2024-12-18 12:00:27 +0700

    Add files via upload

---
+++ Filters.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics_Filters
+{
+    public abstract class Filter
+    {
+        protected abstract Color newPixelColor(Bitmap sourceImage, int x, int y);
+
+        public virtual Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker,int MaxPercent = 100, int add = 0)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+
+            for (int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)((double)i / resultImage.Width * MaxPercent) + add);
+                if (worker.CancellationPending)
+                    return null;
+                for (int j = 0; j < sourceImage.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, newPixelColor(sourceImage, i, j));
+                }
+            }
+
+            return resultImage;
+        }
+
+        protected int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+    }
+
+    public class NegativFilter : Filter
+    {
+        protected override Color newPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            return Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
+        }
+    }
+
+    public class GrayScaleFilter : Filter
+    {
+        protected override Color newPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            int Intensity = (int)(0.299 * sourceColor.R + 0.5876 * sourceColor.G + 0.114 * sourceColor.B);
+            Intensity = Clamp(Intensity, 0, 255);
+            return Color.FromArgb(Intensity, Intensity, Intensity);
+        }
+    }
+
+    public class YarkostFilter : Filter
+    {
+        private int amount;
+
+        public YarkostFilter(int amount)
+        {
+            this.amount = amount;
+        }
+
+        protected override Color newPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            return Color.FromArgb(Clamp(sourceColor.R + amount, 0, 255),
+                Clamp(sourceColor.G + amount, 0, 255),
+                Clamp(sourceColor.B + amount, 0, 255));
+        }
+    }
+
+    public class ContrastFilter : GlobalFilter
+    {
+        protected int brightness = 0;
+
+        private double amount;
+
+        public ContrastFilter(double amount)
+        {
+            this.amount = amount;
+        }
+
+        public override Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker, int MaxPercent = 100, int add = 0)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+
+            brightness = GetBrightness(sourceImage, worker, 50);
+
+            for (int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)((double)i / resultImage.Width * 50) + 50);
+                if (worker.CancellationPending)
+                    return null;
+                for (int j = 0; j < sourceImage.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, newPixelColor(sourceImage, i, j));
+                }
+            }
+
+            return resultImage;
+        }
+
+        protected override Color newPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            double c = amount;
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            return Color.FromArgb(Clamp((int)(brightness + (sourceColor.R - brightness) * c), 0, 255),
+                                  Clamp((int)(brightness + (sourceColor.G - brightness) * c), 0, 255),
+                                  Clamp((int)(brightness + (sourceColor.B - brightness) * c), 0, 255));
+        }
+    }
+
+    public abstract class GlobalFilter : Filter
+    {
+        /// <summary>
+        /// Возвращает среднюю яркость по всем каналам
+        /// </summary>
+        public int GetBrightness(Bitmap sourceImage, BackgroundWorker worker, int MaxPercent = 100)
+        {
+            long brightness = 0;
+            for (int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)((double)i / sourceImage.Width * MaxPercent));
+                if (worker.CancellationPending)
+                    return 0;
+                for (int j = 0; j < sourceImage.Height; j++)
+                {
+                    long pix = 0;
+                    Color color = sourceImage.GetPixel(i, j);
+                    pix += color.R;
+                    pix += color.G;
+                    pix += color.B;
+                    pix /= 3;
+                    brightness += pix;
+                }
+            }
+            brightness /= sourceImage.Width * sourceImage.Height;
+            return (int)brightness;
+        }
+    }
+}
